0cbb25cc79eaf3658bd93644de85510935ee7b05
Anshu Kumar <akgeni@gmail.com>
2021-04-21 14:35:39 +0530

    Create custom_augmenter_textattack.py

---
+++ custom_augmenter_textattack.py
@@ -0,0 +1,12 @@
+from textattack.transformations import WordSwapRandomCharacterDeletion
+from textattack.transformations import CompositeTransformation
+from textattack.augmentation import Augmenter
+transformation = CompositeTransformation([WordSwapRandomCharacterDeletion()])
+augmenter = Augmenter(transformation=transformation, transformations_per_example=5)
+s = 'What I cannot create, I do not understand.'
+
+print(augmenter.augment(s))
+
+#Output
+['What I cannot creae, I do not understand.', 'What I cannot creat, I do not understand.', 
+ 'What I cannot create, I do not nderstand.', 'What I cannot create, I do nt understand.', 'Wht I cannot create, I do not understand.']
